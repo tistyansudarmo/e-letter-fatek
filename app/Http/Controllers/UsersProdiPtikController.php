7a2de05fd30a19c035816f5319ddf8e6519b0ceb
tistyan sudarmo <sudarmoian@gmail.com>
2023-06-14 21:11:43 +0800

    menambahkan autorization

--- app/Http/Controllers/UsersProdiPtikController.php
+++ app/Http/Controllers/UsersProdiPtikController.php
@@ -9,6 +9,10 @@ class UsersProdiPtikController extends Controller
 {
     public function view() {
 
+        if(auth()->user()->prodi_id != 2) {
+            abort(403);
+        }
+
         $user = DB::table('users')
         ->join('levels', 'users.level_id', '=', 'levels.id')
         ->select('users.*', 'levels.jabatan')
--- app/Http/Controllers/UsersProdiSipilController.php
+++ app/Http/Controllers/UsersProdiSipilController.php
@@ -9,6 +9,10 @@ class UsersProdiSipilController extends Controller
 {
     public function view() {
 
+        if(auth()->user()->prodi_id != 3) {
+            abort(403);
+        }
+
         $user = DB::table('users')->join('levels', 'users.level_id', '=', 'levels.id')->select('users.*', 'levels.jabatan')->where('prodi_id', '=', 3)->get();
         return view('layouts.users-prodi.prodi-sipil', ['users' => $user], ['title' => 'Prodi Teknik Sipil']);
     }
